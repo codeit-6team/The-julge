1c408df4af3ba555e40ad3b424f9db1e2e6790fe
Yun-Jinwoo <yjw3480@naver.com>
2025-06-05 23:53:35 +0900

    ✨ feat: 아르바이트 시간 포멧팅 함수 추가

---
+++ src/utils/formatWorkTime.ts
@@ -0,0 +1,16 @@
+function formatWorkTime({ startsAt, workHour }) {
+  const date = new Date(startsAt);
+
+  const kstDate = new Date(date.getTime() + 9 * 60 * 60 * 1000);
+
+  const year = kstDate.getFullYear();
+  const month = String(kstDate.getMonth() + 1).padStart(2, '0');
+  const day = String(kstDate.getDate()).padStart(2, '0');
+  const hours = String(kstDate.getHours()).padStart(2, '0');
+  const afterhours = String(kstDate.getHours() + workHour).padStart(2, '0');
+  const minutes = String(kstDate.getMinutes()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}~${afterhours}:${minutes} `;
+}
+
+export default formatWorkTime;
