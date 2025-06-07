697f61e29af882e1e4af0fb9bcdf02857e2be520
haru <ju30359@gmail.com>
2025-06-07 17:10:17 +0900

    📁 chore : Pagination 컴포넌트 파일 생성

---
+++ src/components/common/pagination/Pagination.tsx
@@ -0,0 +1,8 @@
+import ArrowLeft from '../../../assets/icons/arrow-left.svg';
+import ArrowLeftGray from '../../../assets/icons/arrow-left-gray.svg';
+import ArrowRight from '../../../assets/icons/arrow-right.svg';
+import ArrowRightGray from '../../../assets/icons/arrow-right-gray.svg';
+
+export default function Pagination() {
+  return <div>Pagination</div>;
+}
