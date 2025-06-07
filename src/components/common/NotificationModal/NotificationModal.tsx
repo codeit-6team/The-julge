3d7ede827741fd675bd3cbc2197f2cc112f5dbd0
Yun-Jinwoo <yjw3480@naver.com>
2025-06-07 17:15:17 +0900

    ✨ feat: 닫기 버튼 모바일에서만 보이도록 수정 및 닫기 버튼 클릭시 이전 페이지로 이동

--- src/components/common/NotificationModal/NotificationModal.tsx
+++ src/components/common/NotificationModal/NotificationModal.tsx
@@ -1,12 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import NotificationCard from './NotificationCard';
 import close from '@/assets/icons/ic_close.svg';
 
-export default function NotificationModal({ data, count, onClose }) {
+export default function NotificationModal({ data, count }) {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col px-5 py-10 bg-red-10 gap-4 h-screen md:py-6 md:border-1 md:border-gray-30 md:rounded-[10px] md:shadow-custom md:h-[420px] md:w-[368px]">
       <div className="flex justify-between items-center">
         <h1 className="text-h3 font-bold">알림 {count}개</h1>
-        <button onClick={onClose} className="cursor-pointer">
+        <button onClick={()=> navigate(-1)} className="cursor-pointer md:hidden">
           <img src={close} alt="닫기" />
         </button>
       </div>
