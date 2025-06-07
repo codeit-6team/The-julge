e8a93e841634f49c84899bd209d58bbd2007dea0
Yun-Jinwoo <yjw3480@naver.com>
2025-06-07 16:15:06 +0900

    ✨ feat: NotificationModal 기능 구현 - 데이터 보여주기

--- src/components/common/NotificationModal/NotificationModal.tsx
+++ src/components/common/NotificationModal/NotificationModal.tsx
@@ -1,37 +1,26 @@
 import NotificationCard from './NotificationCard';
 import close from '@/assets/icons/ic_close.svg';
 
-export default function NotificationModal({ data, count }) {
+export default function NotificationModal({ data, count, onClose }) {
   return (
-    <div className="flex flex-col px-5 py-10 bg-red-10 gap-4">
-      <div className="flex justify-between">
-        <h1>알람 {count}개</h1>
-        <button>
+    <div className="flex flex-col px-5 py-10 bg-red-10 gap-4 h-screen md:py-6 md:border-1 md:border-gray-30 md:rounded-[10px] md:shadow-custom md:h-[420px] md:w-[368px]">
+      <div className="flex justify-between items-center">
+        <h1 className="text-h3 font-bold">알림 {count}개</h1>
+        <button onClick={onClose} className="cursor-pointer">
           <img src={close} alt="닫기" />
         </button>
       </div>
-      <div className="flex flex-col gap-2">
-        <NotificationCard
-          status="accepted"
-          restaurantName="Mock Data"
-          startsAt="2025-05-05T13:30:00.000Z"
-          workHour={2}
-          createdAt="2025-05-05T13:30:00.000Z"
-        />
-        <NotificationCard
-          status="rejected"
-          restaurantName="Mock Data"
-          startsAt="2025-05-06T13:30:00.000Z"
-          workHour={3}
-          createdAt="2025-05-06T13:30:00.000Z"
-        />
-        <NotificationCard
-          status="accepted"
-          restaurantName="Mock Data"
-          startsAt="2025-05-13T13:30:00.000Z"
-          workHour={4}
-          createdAt="2025-05-12T13:30:00.000Z"
-        />
+      <div className="flex flex-col gap-2 overflow-y-auto scrollbar-hide">
+        {data.map((data, index) => (
+          <NotificationCard
+            key={index}
+            status={data.item.result}
+            restaurantName={data.item.shop.item.name}
+            startsAt={data.item.notice.item.startsAt}
+            workHour={data.item.notice.item.workhour}
+            createdAt={data.item.createdAt}
+          />
+        ))}
       </div>
     </div>
   );
