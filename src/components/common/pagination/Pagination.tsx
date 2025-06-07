112bcbe8d8a77fc0eefc300b79530158594a005c
haru <ju30359@gmail.com>
2025-06-07 17:18:06 +0900

    ✨ feat: Pagination 컴포넌트 기본 구조 및 Props 적용

--- src/components/common/pagination/Pagination.tsx
+++ src/components/common/pagination/Pagination.tsx
@@ -3,6 +3,22 @@ import ArrowLeftGray from '../../../assets/icons/arrow-left-gray.svg';
 import ArrowRight from '../../../assets/icons/arrow-right.svg';
 import ArrowRightGray from '../../../assets/icons/arrow-right-gray.svg';
 
-export default function Pagination() {
-  return <div>Pagination</div>;
+interface Props {
+  totalPages: number;
+  currentPage: number;
+  onChangePage: (pageNumber: number) => void;
+}
+
+export default function Pagination({
+  totalPages,
+  currentPage,
+  onChangePage,
+}: Props) {
+  return (
+    <div>
+      <p>총 페이지 수 : {totalPages}</p>
+      <p>현재 페이지: {currentPage}</p>
+      <button onClick={() => onChangePage(currentPage + 1)}>다음</button>
+    </div>
+  );
 }
