afa3f21a43bd1ced156e13fda74fe08964b5113e
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-07 16:17:44 +0900

    ✨ feat: Props type 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,10 @@
+import type { ButtonHTMLAttributes } from 'react';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  solid?: boolean;
+  size?: 'large' | 'medium' | 'small';
+}
+
 export default function Button() {
   return;
 }
