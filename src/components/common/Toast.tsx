8ed33f5ef076b1728663d3f1d25d5bf55fbf0367
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-08 19:32:28 +0900

    ✨ feat: props 추가

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,3 +1,5 @@
-export default function Toast() {
-  return;
+import type { HTMLAttributes } from 'react';
+
+export default function Toast({ ...props }: HTMLAttributes<HTMLElement>) {
+  return <div {...props}></div>;
 }
