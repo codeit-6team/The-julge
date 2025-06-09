ff116044d3e4a4802af31e149f40ed32519852e9
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-09 17:32:09 +0900

    ✨ feat: className prop 추가

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,8 +1,13 @@
 import type { HTMLAttributes } from 'react';
 
 export default function Toast({
+  className = '',
   children,
   ...props
 }: HTMLAttributes<HTMLElement>) {
-  return <div {...props}>{children}</div>;
+  return (
+    <div className={`${className}`} {...props}>
+      {children}
+    </div>
+  );
 }
