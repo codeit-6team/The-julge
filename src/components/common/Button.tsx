c75764c52225bc52addb981fad78d0edbab95174
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-10 14:58:18 +0900

    ✨ feat: type prop 기본값 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -9,12 +9,14 @@ export default function Button({
   solid = true,
   size = 'large',
   className = '',
+  type = 'button',
   children,
   ...props
 }: Props) {
   return (
     <button
       className={`rounded-md disabled:border-none disabled:bg-gray-40 disabled:text-white ${solid ? 'bg-primary text-white' : 'border border-primary bg-white text-primary'} ${size === 'large' ? 'h-48 text-body1 font-bold' : size === 'medium' ? 'h-37 text-body2 font-bold' : 'h-32 text-caption font-regular'} ${className}`}
+      type={type}
       {...props}
     >
       {children}
