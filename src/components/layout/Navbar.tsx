ada074bab738631c9fb9adc656c1c02bd46267bc
Yun-Jinwoo <yjw3480@naver.com>
2025-06-11 00:48:31 +0900

    🐛 fix: 맡은 파일 포멧팅 적용

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
   // 바깥 클릭 시 모달 닫기
   useEffect(() => {
     function handleClickOutside(event: MouseEvent): void {
-      const target = event.target as Node
+      const target = event.target as Node;
       if (
         isShowModal &&
         modalRef.current &&
@@ -44,31 +44,35 @@ export default function Navbar() {
 
   return (
     <header className="bg-white">
-      <nav className="relative flex flex-wrap items-center justify-between gap-y-22 py-10 md:py-15 mx-20 md:mx-32 lg:max-w-1023 lg:mx-auto">
-        <Link to="/" className='h-30 md:h-40 order-1 flex items-center justify-center'>
-          <img
-            src={logo}
-            alt="더줄게 로고 이미지"
-            className="h-15 md:h-20"
-          />
+      <nav className="relative mx-20 flex flex-wrap items-center justify-between gap-y-22 py-10 md:mx-32 md:py-15 lg:mx-auto lg:max-w-1023">
+        <Link
+          to="/"
+          className="order-1 flex h-30 items-center justify-center md:h-40"
+        >
+          <img src={logo} alt="더줄게 로고 이미지" className="h-15 md:h-20" />
         </Link>
 
-        <div className="relative order-4 w-full md:flex-1 md:order-2 md:ml-40 ">
+        <div className="relative order-4 w-full md:order-2 md:ml-40 md:flex-1">
           <img
             src={search}
-            className="absolute -translate-y-1/2 left-10 top-1/2"
+            className="absolute top-1/2 left-10 -translate-y-1/2"
           />
           <input
             type="text"
             placeholder="가게 이름으로 찾아보세요"
-            className="w-full md:w-344 lg:w-450 h-36 md:h-40 bg-gray-10 pl-40 pt-10 pb-10 rounded-[10px] placeholder:text-body2 placeholder:text-gray-40"
+            className="h-36 w-full rounded-[10px] bg-gray-10 pt-10 pb-10 pl-40 placeholder:text-body2 placeholder:text-gray-40 md:h-40 md:w-344 lg:w-450"
           />
         </div>
         {isLoggedIn ? (
-          <div className="flex items-center gap-16 font-bold text-body2 md:gap-12 md:text-body1 order-3 lg:gap-40">
-            <Link to={role === 'employer' ? '/owner/store' : '/profile'}>{role === 'employer' ? '내 가게' : '내 프로필'}</Link>
+          <div className="order-3 flex items-center gap-16 text-body2 font-bold md:gap-12 md:text-body1 lg:gap-40">
+            <Link to={role === 'employer' ? '/owner/store' : '/profile'}>
+              {role === 'employer' ? '내 가게' : '내 프로필'}
+            </Link>
             <button onClick={logout}>로그아웃</button>
-            <button ref={buttonRef} onClick={()=> setIsShowModal(prev => !prev)}>
+            <button
+              ref={buttonRef}
+              onClick={() => setIsShowModal((prev) => !prev)}
+            >
               {alarms.count > 0 ? (
                 <img src={alarmActive} />
               ) : (
@@ -77,20 +81,24 @@ export default function Navbar() {
             </button>
           </div>
         ) : (
-          <div className="flex gap-16 font-bold text-body2 md:gap-40 md:text-body1 order-3">
+          <div className="order-3 flex gap-16 text-body2 font-bold md:gap-40 md:text-body1">
             <Link to="/login">로그인</Link>
             <Link to="/signup">회원가입</Link>
           </div>
         )}
 
         {isShowModal && (
-        <div
-          className="absolute z-100 top-0 -left-20 md:top-56 md:right-0 md:left-auto"
-          ref={modalRef}
-        >
-          <NotificationModal data={alarms.items} count={alarms.count} onClose={() => setIsShowModal(false)}/>
-        </div>
-      )}
+          <div
+            className="absolute top-0 -left-20 z-100 md:top-56 md:right-0 md:left-auto"
+            ref={modalRef}
+          >
+            <NotificationModal
+              data={alarms.items}
+              count={alarms.count}
+              onClose={() => setIsShowModal(false)}
+            />
+          </div>
+        )}
       </nav>
     </header>
   );
