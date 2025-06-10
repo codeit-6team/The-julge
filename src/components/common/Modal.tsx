f3555d3f46d21bb1075a11b11c65e295500cd0f8
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-10 14:29:02 +0900

    ✨ feat: props 추가

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,3 +1,21 @@
-export default function Modal() {
+import type { MouseEventHandler } from 'react';
+
+interface Props {
+  /** 기본 값은 alert */
+  option?: 'alert' | 'confirm' | 'action';
+  /** button click event handler */
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  /** option이 action일 때만 사용. 아니오 button에 해당하는 click event handler. */
+  onClickNo?: MouseEventHandler<HTMLButtonElement>;
+  /** option이 alert일 때만 사용. */
+  content?: string;
+}
+
+export default function Modal({
+  option = 'alert',
+  onClick,
+  onClickNo,
+  content,
+}: Props) {
   return <div className="fixed inset-0 z-40 bg-[#000] opacity-70"></div>;
 }
