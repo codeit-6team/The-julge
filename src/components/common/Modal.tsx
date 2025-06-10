e7c0a3ac586cf26c60206f732bdb84ec9645c8da
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-10 18:03:49 +0900

    ✨ feat: modal에 icon 추가

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,4 +1,6 @@
 import type { MouseEventHandler } from 'react';
+import ic_check from '@/assets/icons/modal_check.svg';
+import ic_exclamation from '@/assets/icons/modal_exclamation.svg';
 
 interface Props {
   /** 기본 값은 alert */
@@ -15,13 +17,23 @@ export default function Modal({
   option = 'alert',
   onClick,
   onClickNo,
-  content,
+  content = '안녕',
 }: Props) {
   return (
     <div className="fixed inset-0 z-40 content-center justify-items-center bg-[#000000b3]">
       <div
         className={`flex flex-col items-center justify-between bg-white ${option === 'alert' ? 'h-220 w-327 rounded-lg p-28 md:h-250 md:w-540' : 'h-184 w-298 rounded-xl p-24'}`}
-      ></div>
+      >
+        <div
+          className={`flex flex-col items-center gap-16 ${option === 'alert' ? 'mt-53 md:mt-80' : ''}`}
+        >
+          {option === 'confirm' ? (
+            <img src={ic_exclamation} />
+          ) : option === 'action' ? (
+            <img src={ic_check} />
+          ) : null}
+        </div>
+      </div>
     </div>
   );
 }
