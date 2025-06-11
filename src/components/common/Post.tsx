4d203e9a864f9c0ca3403353bea547557724bbdb
박광민 <rhkdals0242@naver.com>
2025-06-11 20:36:40 +0900

    ✨ feat: PostProps 타입 추가

--- src/components/common/Post.tsx
+++ src/components/common/Post.tsx
@@ -1,3 +1,12 @@
-export default function Post() {
-  return;
+interface PostProps {
+  imageUrl: string;
+  title: string;
+  startAt: string; // 근무 시작 시간
+  workhour: number; // 근무 시간
+  location: string;
+  hourlyPay: number; // 시급
+  originalHourlyPay: number; // 전에 등록한 시급
+  closed: boolean; // 마감 여부
 }
+
+export default function Post() {}
