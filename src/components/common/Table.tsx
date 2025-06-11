26f36db3986bf4357a576579107d8d8257c3373f
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-11 16:53:13 +0900

    ✨ feat: 외부 데이터 type 임시 설정
    - 추후에 api 파일 등에서 import로 변경 예정

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -1,3 +1,54 @@
+interface User {
+  item: {
+    id: string;
+    email: string;
+    type: 'employer' | 'employee';
+    name?: string;
+    phone?: string;
+    address?: string;
+    bio?: string;
+  };
+  href: string;
+}
+
+interface Shop {
+  item: {
+    id: string;
+    name: string;
+    category: string;
+    address1: string;
+    address2: string;
+    description: string;
+    imageUrl: string;
+    originalHourlyPay: number;
+  };
+  href: string;
+}
+
+interface Notice {
+  item: {
+    id: string;
+    hourlyPay: number;
+    description: string;
+    startsAt: string;
+    workhour: number;
+    closed: boolean;
+  };
+  href: string;
+}
+
+interface Item {
+  item: {
+    id: string;
+    status: 'pending' | 'accepted' | 'rejected' | 'canceled';
+    createdAt: string;
+    user?: User;
+    shop: Shop;
+    notice: Notice;
+  };
+  links: string[];
+}
+
 export default function Table() {
   return (
     <table>
