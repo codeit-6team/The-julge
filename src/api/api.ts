03ae1c18db891eef86afa4ec835b45f1380fbc65
Yun-Jinwoo <yjw3480@naver.com>
2025-06-12 00:35:17 +0900

    ✨ feat: Authorization이 필요하지 않은 요청에 대해서는 인터셉터에서  Authorization 헤더를 추가하지 않도록 처리

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const publicPaths = ['/login', '/signup'];
-
 const api = axios.create({
   baseURL: 'https://bootcamp-api.codeit.kr/api/15-6/the-julge', // 공통 prefix
   headers: {
@@ -9,10 +7,31 @@ const api = axios.create({
   },
 });
 
+// 가독성을 위해 prettier-ignore 적용
+//prettier-ignore
+function isPublicRequest(method?: string, url?: string): boolean {
+  if (!method || !url) return false;
+
+  return (
+    method === 'get' &&
+      (
+        url?.startsWith('/notices') ||
+        url?.startsWith('/shops')
+      )) ||
+    (method === 'post' &&
+      (
+        url === '/token' ||
+        url === '/users'
+      )
+    );
+}
+
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
-    const isPublic = publicPaths.some((path) => config.url?.includes(path));
+    const { url, method } = config;
+
+    const isPublic = isPublicRequest(method, url);
     if (!isPublic && token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
