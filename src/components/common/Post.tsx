47a57cbb20f3f5520d5bcc8b09bdff211efd9f64
박광민 <rhkdals0242@naver.com>
2025-06-11 22:24:45 +0900

    ✨ feat: 날짜 포맷 함수 추가

--- src/components/common/Post.tsx
+++ src/components/common/Post.tsx
@@ -22,4 +22,21 @@ function getStatus(
   return 'ACTIVE';
 }
 
+// 날짜 포맷
+function formatDate(startAt: string, workhour: number): string {
+  const start = new Date(startAt);
+  const end = new Date(start.getTime() + workhour * 60 * 60 * 1000);
+
+  const yyyy = start.getFullYear();
+  const mm = String(start.getMonth() + 1).padStart(2, '0'); // padStart 자리 수를 맞추는 용도
+  const dd = String(start.getDate()).padStart(2, '0');
+  const startHour = String(start.getHours()).padStart(2, '0');
+  const startMin = String(start.getMinutes()).padStart(2, '0');
+
+  const endHour = String(end.getHours()).padStart(2, '0');
+  const endMin = String(end.getMinutes()).padStart(2, '0');
+
+  return `${yyyy}-${mm}-${dd} ${startHour}:${startMin}~${endHour}:${endMin}`;
+}
+
 export default function Post() {}
