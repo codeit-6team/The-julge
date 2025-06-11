97c0d67d1a81c538490714d70ef57adf01a5dc10
Yun-Jinwoo <yjw3480@naver.com>
2025-06-11 16:51:03 +0900

    ✨ feat: postUser 함수 타입 추가

--- src/api/postUser.ts
+++ src/api/postUser.ts
@@ -8,10 +8,23 @@ interface ErrorMessage {
 interface SignupParams {
   email: string;
   password: string;
-  type: string;
+  type: employee | employer;
 }
 
-export const postUser = async ({ email, password, type }: SignupParams) => {
+interface SignupResponse {
+  item: {
+    id: string;
+    email: string;
+    type: employee | employer;
+  };
+  links: object[]; // 정확한 타입이 나와있지 않아서 우선 object로만 처리함
+}
+
+export const postUser = async ({
+  email,
+  password,
+  type,
+}: SignupParams): Promise<SignupResponse> => {
   try {
     const response = await api.post('/users', {
       email,
@@ -20,7 +33,7 @@ export const postUser = async ({ email, password, type }: SignupParams) => {
     });
     return response.data;
   } catch (error) {
-    const axiosError = error as AxiosError<ErrorMessage>; // 에러 타입 명시
+    const axiosError = error as AxiosError<ErrorMessage, SignupParams>; // 에러 타입 명시
     if (axiosError.response) {
       throw new Error(axiosError.response.data.message);
     } else {
