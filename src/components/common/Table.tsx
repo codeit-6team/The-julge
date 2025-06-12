860e2f1a4885fa32682247b3c88323b3a0ada22b
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-12 22:49:06 +0900

    🎨 style: 상태 태그 스타일 적용

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -163,14 +163,22 @@ export default function Table({
                     </Button>
                   </div>
                 ) : (
-                  <div>대기중</div>
+                  <div className="inline-block rounded-full bg-green-10 px-10 py-6 text-caption/16 text-green-20 md:text-body2/17 md:font-bold">
+                    대기중
+                  </div>
                 )
               ) : data[3] === 'accepted' ? (
-                <div>승인 완료</div>
+                <div className="inline-block rounded-full bg-blue-10 px-10 py-6 text-caption/16 text-blue-20 md:text-body2/17 md:font-bold">
+                  승인 완료
+                </div>
               ) : data[3] === 'rejected' ? (
-                <div>거절</div>
+                <div className="inline-block rounded-full bg-red-10 px-10 py-6 text-caption/16 text-red-40 md:text-body2/17 md:font-bold">
+                  거절
+                </div>
               ) : data[3] === 'canceled' ? (
-                <div>취소</div>
+                <div className="inline-block rounded-full bg-gray-20 px-10 py-6 text-caption/16 text-gray-50 md:text-body2/17 md:font-bold">
+                  취소
+                </div>
               ) : null}
             </td>
           </tr>
