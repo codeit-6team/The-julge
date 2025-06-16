0c87989630a1297f82aa0278285237ac58d7983d
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-16 14:11:47 +0900

    ✨ feat: shopApi ShopItem type 추가

--- src/api/shopApi.ts
+++ src/api/shopApi.ts
@@ -4,3 +4,14 @@ export interface LinkInfo {
   method: string;
   href: string;
 }
+
+export interface ShopItem {
+  id: string;
+  name: string;
+  category: string;
+  address1: string;
+  address2: string;
+  description: string;
+  imageUrl: string;
+  originalHourlyPay: number;
+}
