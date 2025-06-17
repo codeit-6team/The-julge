dd640d7dcedf9a22a4395347260c846b8b79b977
Yun-Jinwoo <yjw3480@naver.com>
2025-06-17 16:11:48 +0900

    ✨ feat: 모달 컴포넌트에 ref 속성 추가

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -14,6 +14,8 @@ interface Props {
   yesButtonContent?: string;
   /** modal 문구 */
   children?: ReactNode;
+  /** 부모로부터 전달받을 ref */
+  ref?: Ref<HTMLDivElement>;
 }
 
 export default function Modal({
@@ -22,11 +24,15 @@ export default function Modal({
   onYesButtonClick,
   yesButtonContent,
   children,
+  ref,
 }: Props) {
   return (
     <div className="fixed inset-0 z-40 content-center justify-items-center bg-[#000000b3]">
       {option === 'alert' ? (
-        <div className="flex h-220 w-327 flex-col items-center justify-between rounded-lg bg-white p-28 md:h-250 md:w-540">
+        <div
+          ref={ref}
+          className="flex h-220 w-327 flex-col items-center justify-between rounded-lg bg-white p-28 md:h-250 md:w-540"
+        >
           <div className="mt-53 text-center text-body1/19 font-regular text-[#333236] md:mt-80 md:text-[18px]/21">
             {children}
           </div>
@@ -38,7 +44,10 @@ export default function Modal({
           </button>
         </div>
       ) : (
-        <div className="flex h-184 w-298 flex-col items-center justify-between rounded-xl bg-white p-24">
+        <div
+          ref={ref}
+          className="flex h-184 w-298 flex-col items-center justify-between rounded-xl bg-white p-24"
+        >
           <div className="flex flex-col items-center gap-16">
             {option === 'action' ? (
               <img src={ic_check} />
