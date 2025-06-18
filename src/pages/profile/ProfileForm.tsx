48665c5b77a1efa0e55a604039a23d8cac189f5f
Yun-Jinwoo <yjw3480@naver.com>
2025-06-18 17:39:35 +0900

    🎨 style: 데스크탑/테블릿용 반응형 페이지 구현

--- src/pages/profile/ProfileForm.tsx
+++ src/pages/profile/ProfileForm.tsx
@@ -7,31 +7,36 @@ import { ADDRESS_OPTIONS } from '@/constants/dropdownOptions';
 
 export default function ProfileForm() {
   return (
-    <form className="mx-12 mt-40 mb-80 flex flex-col gap-24">
-      <div className="flex items-center justify-between">
-        <h1 className="text-h3 font-bold">내 프로필</h1>
-        <Link to="/profile">
-          <img src={close} alt="닫기" />
-        </Link>
-      </div>
-      <div className="flex flex-col gap-20">
-        <Input label="이름*" />
-        <Input label="연락처*" />
-        <div className="flex flex-col gap-8 text-body1/26 font-regular">
-          <label htmlFor="region">선호 지역</label>
-          <Dropdown id="region" variant="form" options={ADDRESS_OPTIONS} />
+    <div className="min-h-screen bg-gray-5">
+      <form className="mx-12 flex flex-col gap-24 pt-40 pb-80 md:mx-32 md:gap-32 md:py-60 lg:mx-auto lg:w-964">
+        <div className="flex items-center justify-between">
+          <h1 className="text-h3 font-bold md:text-h1">내 프로필</h1>
+          <Link to="/profile">
+            <img src={close} alt="닫기" className="md:size-32" />
+          </Link>
         </div>
-        <div className="flex flex-col gap-8 text-body1/26 font-regular">
-          <label htmlFor="bio">소개</label>
-          <textarea
-            name="bio"
-            id="bio"
-            className="h-153 resize-none rounded-[5px] border border-gray-30 px-20 py-16"
-            placeholder="입력"
-          ></textarea>
+        <div className="flex flex-col gap-20 md:gap-24">
+          <div className="grid grid-cols-1 gap-20 md:grid-cols-2 md:gap-y-24 lg:grid-cols-3">
+            <Input label="이름*" />
+            <Input label="연락처*" />
+            <div className="flex flex-col gap-8 text-body1/26 font-regular">
+              <label htmlFor="region">선호 지역</label>
+              <Dropdown id="region" variant="form" options={ADDRESS_OPTIONS} />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-8 text-body1/26 font-regular">
+            <label htmlFor="bio">소개</label>
+            <textarea
+              name="bio"
+              id="bio"
+              className="h-153 resize-none rounded-[5px] border border-gray-30 bg-white px-20 py-16 placeholder-gray-40"
+              placeholder="입력"
+            ></textarea>
+          </div>
         </div>
-      </div>
-      <Button>등록하기</Button>
-    </form>
+        <Button className="md:mx-auto md:w-312">등록하기</Button>
+      </form>
+    </div>
   );
 }
