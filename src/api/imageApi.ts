57934f68fed13b5bf84a54f7f8a90543d9dfedc7
박광민 <rhkdals0242@naver.com>
2025-06-19 19:36:07 +0900

    ✨ feat: 이미지 API 추가 및 presigned URL 발급 기능 구현

---
+++ src/api/imageApi.ts
@@ -0,0 +1,52 @@
+import api from './api';
+import { AxiosError } from 'axios';
+import type { LinkInfo } from './shopApi';
+
+interface ErrorMessage {
+  message: string;
+}
+
+interface ImageRequest {
+  name: string;
+}
+
+interface ImageResponse {
+  item: {
+    url: string; // ✅ 쿼리 스트링 포함된 presigned S3 PUT URL
+  };
+  links: LinkInfo[];
+}
+
+// Presigned URL 발급 요청
+export const getPresignedUrl = async (filename: string): Promise<string> => {
+  try {
+    const body: ImageRequest = { name: filename };
+    const response = await api.post<ImageResponse>('/images', body);
+    return response.data.item.url;
+  } catch (error) {
+    const axiosError = error as AxiosError<ErrorMessage>; // 에러 타입 명시
+    if (axiosError.response) {
+      throw new Error(axiosError.response.data.message);
+    } else {
+      throw new Error('서버에 연결할 수 없습니다. 인터넷 연결을 확인해주세요.');
+    }
+  }
+};
+
+// S3 업로드
+export const uploadImageToS3 = async (
+  uploadUrl: string,
+  file: File,
+): Promise<void> => {
+  const response = await fetch(uploadUrl, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': file.type,
+    },
+    body: file,
+  });
+
+  if (!response.ok) {
+    throw new Error('이미지 업로드를 실패했습니다.');
+  }
+};
