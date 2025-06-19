1999fdd382c181d510638aa411e94aaa7019872b
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-19 14:15:05 +0900

    ✨ feat: 하단(등록한 공고) section 태그 생성

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -17,6 +17,7 @@ export default function Store() {
   return (
     <>
       <section className="w-full bg-white"></section>
+      {shop && <section className="w-full bg-gray-5"></section>}
     </>
   );
 }
