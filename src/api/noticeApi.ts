19e2f93c437a3e66d801f5832c5a339e7c8bb096
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-19 16:41:07 +0900

    ✨ feat: noticeApi postShopNotice api 함수 추가

--- src/api/noticeApi.ts
+++ src/api/noticeApi.ts
@@ -141,3 +141,24 @@ export const getShopNotices = async (
     }
   }
 };
+
+// POST /shops/{shop_id}/notices 공고 등록
+export const postShopNotice = async (
+  shopId: string,
+  body: NoticeUpsertRequest,
+): Promise<NoticeUpsertResponse> => {
+  try {
+    const response = await api.post<NoticeUpsertResponse>(
+      `/shops/${shopId}/notices`,
+      body,
+    );
+    return response.data;
+  } catch (error) {
+    const axiosError = error as AxiosError<ErrorMessage>; // 에러 타입 명시
+    if (axiosError.response) {
+      throw new Error(axiosError.response.data.message);
+    } else {
+      throw new Error('서버에 연결할 수 없습니다. 인터넷 연결을 확인해주세요.');
+    }
+  }
+};
