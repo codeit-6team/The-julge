ac86ad2726226d838e1f0d879785838bbdf34ab3
Yun-Jinwoo <yjw3480@naver.com>
2025-06-20 11:26:38 +0900

    ♻️ refactor: 헤더 삭제, 박스 부분만 담당

--- src/components/layout/RegisterLayout.tsx
+++ src/components/layout/RegisterLayout.tsx
@@ -3,25 +3,21 @@ import Button from '../common/Button';
 
 const TEXT_MAP = {
   profile: {
-    header: '내 프로필',
     content: '내 프로필을 등록하고 원하는 가게에 지원해 보세요.',
     buttonText: '내 프로필 등록하기',
     link: '/profile/edit',
   },
   store: {
-    header: '내 가게',
     content: '내 가게를 소개하고 공고도 등록해 보세요.',
     buttonText: '가게 등록하기',
     link: '/owner/store/edit',
   },
   application: {
-    header: '신청 내역',
     content: '아직 신청 내역이 없어요.',
     buttonText: '공고 보러가기',
     link: '/',
   },
   notice: {
-    header: '등록한 공고',
     content: '공고를 등록해 보세요.',
     buttonText: '공고 등록하기',
     link: '/owner/post',
@@ -34,21 +30,18 @@ export default function RegisterLayout({
   type: 'profile' | 'store' | 'application' | 'notice';
 }) {
   const navigate = useNavigate();
-  const { header, content, buttonText, link } = TEXT_MAP[type];
+  const { content, buttonText, link } = TEXT_MAP[type];
 
   return (
-    <div className="mx-12 flex flex-col gap-16 pt-40 pb-80 md:mx-32 md:gap-24 md:pt-60 md:pb-120 lg:mx-auto lg:w-964">
-      <h1 className="text-h3 font-bold md:text-h1">{header}</h1>
-      <div className="flex flex-col items-center gap-16 rounded-[12px] border border-gray-20 px-24 py-60 md:gap-24">
-        <p className="text-body2/22 md:text-body1/26">{content}</p>
-        <Button
-          size="medium"
-          className="px-20 py-10 md:h-48 md:w-344 md:text-body1 md:font-bold"
-          onClick={() => navigate(link)}
-        >
-          {buttonText}
-        </Button>
-      </div>
+    <div className="flex flex-col items-center gap-16 rounded-[12px] border border-gray-20 px-24 py-60 md:gap-24">
+      <p className="text-body2/22 md:text-body1/26">{content}</p>
+      <Button
+        size="medium"
+        className="px-20 py-10 md:h-48 md:w-344 md:text-body1 md:font-bold"
+        onClick={() => navigate(link)}
+      >
+        {buttonText}
+      </Button>
     </div>
   );
 }
