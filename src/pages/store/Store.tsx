8dacbdd5387ec59c539795764d5b5bf1c63eb6e4
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-20 15:44:40 +0900

    ✨ feat: 무한 스크롤 로직 추가
    test 부족

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -11,9 +11,11 @@ export default function Store() {
   const navigate = useNavigate();
   const [shop, setShop] = useState<ShopItem | null>(null);
   const [notices, setNotices] = useState<NoticeInfo[]>([]);
+  const noticesOffset = useRef(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState('');
-  const noticesOffset = useRef(0);
+  const [isLoading, setIsLoading] = useState<boolean | 'error'>(false);
+  const observerRef = useRef<HTMLDivElement>(null);
 
   const handleClose = useCallback(() => {
     if (modalContent.startsWith('로그인')) {
@@ -25,13 +27,19 @@ export default function Store() {
 
   const loadNotices = useCallback(async () => {
     if (!shop || noticesOffset.current < 0) return;
-    const noticesResponse = await getShopNotices(shop.id, {
-      offset: noticesOffset.current,
-      limit: NOTICES_LIMIT,
-    });
-    setNotices((prev) => [...prev, ...noticesResponse.items]);
-    if (noticesResponse.hasNext) noticesOffset.current += NOTICES_LIMIT;
-    else noticesOffset.current = -1;
+    setIsLoading(true);
+    try {
+      const noticesResponse = await getShopNotices(shop.id, {
+        offset: noticesOffset.current,
+        limit: NOTICES_LIMIT,
+      });
+      setNotices((prev) => [...prev, ...noticesResponse.items]);
+      if (noticesResponse.hasNext) noticesOffset.current += NOTICES_LIMIT;
+      else noticesOffset.current = -1;
+      setIsLoading(false);
+    } catch {
+      setIsLoading('error');
+    }
   }, [shop]);
 
   useEffect(() => {
@@ -53,6 +61,26 @@ export default function Store() {
     }
   }, []);
 
+  // 무한 스크롤
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && !isLoading) {
+          loadNotices();
+        }
+      },
+      {
+        rootMargin: '300px',
+      },
+    );
+
+    const { current } = observerRef;
+    if (current) observer.observe(current);
+    return () => {
+      if (current) observer.unobserve(current);
+    };
+  }, [loadNotices, isLoading]);
+
   return (
     <>
       <section className="w-full bg-white px-12 py-40 md:px-32 md:py-60">
@@ -61,6 +89,15 @@ export default function Store() {
       {shop && (
         <section className="w-full bg-gray-5 px-12 pt-40 pb-80 md:px-32 md:pt-60 md:pb-120">
           <div className="mx-auto max-w-964"></div>
+          {/* 스크롤 감지 요소 */}
+          <div
+            className="text-center text-caption text-gray-30"
+            ref={observerRef}
+          >
+            {isLoading === 'error'
+              ? '데이터를 불러오는데 실패했습니다.'
+              : isLoading && '로딩 중...'}
+          </div>
         </section>
       )}
       {isModalOpen && (
