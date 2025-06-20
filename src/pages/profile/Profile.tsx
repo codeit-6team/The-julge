e8cbae0ef48314ec32351c2b8d70f909f1e5b2fd
Yun-Jinwoo <yjw3480@naver.com>
2025-06-20 18:01:36 +0900

    ✨ feat: 프로필 상세 페이지의 신청 내역 부분 구현

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,9 +1,12 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '@/context/AuthContext';
 import RegisterLayout from '@/components/layout/RegisterLayout';
+import Table from '@/components/common/table/Table';
 import Button from '@/components/common/Button';
+import Modal from '@/components/common/Modal';
 import { getUser } from '@/api/userApi';
+import { getUserApplications } from '@/api/applicationApi';
 import formatPhone from '@/utils/formatPhone';
 import phone from '@/assets/icons/phone.svg';
 import location from '@/assets/icons/location-red.svg';
@@ -17,7 +20,8 @@ export default function Profile() {
     address: '',
     bio: '',
   });
-  const [isExist, setIsExist] = useState(false);
+  const [hasInfo, setHasInfo] = useState(false);
+  const [hasApplications, setHasApplications] = useState(false);
   const [modal, setModal] = useState({
     isOpen: false,
     message: '',
@@ -59,8 +63,9 @@ export default function Profile() {
           address: userInfo.item.address,
           bio: userInfo.item.bio,
         });
-
-        setIsExist(!!userInfo.item.name);
+        setHasInfo(!!userInfo.item.name);
+        const applications = await getUserApplications(userId);
+        setHasApplications(applications.count > 0);
       } catch (error) {
         setModal({
           isOpen: true,
@@ -71,47 +76,75 @@ export default function Profile() {
     fetchUserInfo();
   }, [isLoggedIn]);
 
+  const handleModalConfirm = useCallback(() => {
+    if (modal.message.includes('로그인')) {
+      setModal({ isOpen: false, message: '' });
+      navigate('/login');
+    } else {
+      setModal({ isOpen: false, message: '' });
+    }
+  }, [modal.message, navigate]);
+
   return (
-    <div className="mx-12 flex flex-col gap-16 pt-40 pb-80 md:mx-32 md:gap-24 md:pt-60 md:pb-120 lg:mx-auto lg:w-964 lg:flex-row lg:justify-between lg:gap-180">
-      <h1 className="text-h3 font-bold md:text-h1">내 프로필</h1>
-      {!isExist ? (
-        <div>
+    <div className="min-h-[calc(100vh-102px)] md:min-h-[calc(100vh-70px)]">
+      {!hasInfo ? (
+        <div className="mx-12 flex flex-col gap-16 py-40 md:mx-32 md:gap-24 md:py-60 lg:mx-auto lg:w-964">
+          <h1 className="text-h3 font-bold md:text-h1">내 프로필</h1>
           <RegisterLayout type="profile" />
         </div>
       ) : (
-        <div className="flex justify-between rounded-[12px] bg-red-10 p-20 md:p-32 lg:flex-1">
-          <div className="flex flex-col gap-8 md:gap-12">
-            <div className="flex flex-col gap-8">
-              <div className="text-body2/17 font-bold text-primary md:text-body1/20">
-                이름
+        <div className="mx-12 flex flex-col gap-16 py-40 md:mx-32 md:gap-24 md:py-60 lg:mx-auto lg:w-964 lg:flex-row lg:justify-between lg:gap-180">
+          <h1 className="text-h3 font-bold md:text-h1">내 프로필</h1>
+          <div className="flex justify-between rounded-[12px] bg-red-10 p-20 md:p-32 lg:flex-1">
+            <div className="flex flex-col gap-8 md:gap-12">
+              <div className="flex flex-col gap-8">
+                <div className="text-body2/17 font-bold text-primary md:text-body1/20">
+                  이름
+                </div>
+                <h2 className="text-h2/29 font-bold md:text-h1/34">
+                  {profileInfo.name}
+                </h2>
               </div>
-              <h2 className="text-h2/29 font-bold md:text-h1/34">
-                {profileInfo.name}
-              </h2>
+              <div className="flex items-center gap-6 text-body2/22 text-gray-50 md:text-body1/26">
+                <img src={phone} className="size-16 md:size-20" />
+                {formatPhone(profileInfo.phone)}
+              </div>
+              <div className="flex items-center gap-6 text-body2/22 text-gray-50 md:text-body1/26">
+                <img src={location} className="size-16 md:size-20" />
+                선호 지역: {profileInfo.address}
+              </div>
+              {profileInfo.bio && (
+                <p className="mt-12 text-body2/22 md:mt-16 md:text-body1/26">
+                  {profileInfo.bio}
+                </p>
+              )}
             </div>
-            <div className="flex items-center gap-6 text-body2/22 text-gray-50 md:text-body1/26">
-              <img src={phone} className="size-16 md:size-20" />
-              {formatPhone(profileInfo.phone)}
-            </div>
-            <div className="flex items-center gap-6 text-body2/22 text-gray-50 md:text-body1/26">
-              <img src={location} className="size-16 md:size-20" />
-              선호 지역: {profileInfo.address}
-            </div>
-            {profileInfo.bio && (
-              <p className="mt-12 text-body2/22 md:mt-16 md:text-body1/26">
-                {profileInfo.bio}
-              </p>
-            )}
+            <Button
+              size={buttonSize}
+              solid={false}
+              onClick={() => navigate('/profile/edit')}
+              className="w-108 shrink-0 md:w-169"
+            >
+              편집하기
+            </Button>
           </div>
-          <Button
-            size={buttonSize}
-            solid={false}
-            onClick={() => navigate('/profile/edit')}
-            className="w-108 shrink-0 md:w-169"
-          >
-            편집하기
-          </Button>
         </div>
+      )}
+
+      <div className="bg-gray-5">
+        <div className="mx-12 flex flex-col gap-16 pt-40 pb-80 md:mx-32 md:gap-24 md:pt-60 md:pb-120 lg:mx-auto lg:w-964">
+          <h1 className="text-h3 font-bold md:text-h1">신청 내역</h1>
+          {!hasApplications ? (
+            <RegisterLayout type="application" />
+          ) : (
+            <Table mode="user" userId={localStorage.getItem('userId')} />
+          )}
+        </div>
+      </div>
+      {modal.isOpen && (
+        <Modal onClose={handleModalConfirm} onButtonClick={handleModalConfirm}>
+          {modal.message}
+        </Modal>
       )}
     </div>
   );
