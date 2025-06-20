7a8f7c74ba99718302ddb7d69c270b4d6b8c4c49
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-20 13:19:34 +0900

    ✨ feat: 비로그인 상태를 위한 Modal 추가

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,13 +1,24 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getUser } from '@/api/userApi';
 import type { ShopItem } from '@/api/shopApi';
+import Modal from '@/components/common/Modal';
 
 export default function Store() {
+  const navigate = useNavigate();
   const [shop, setShop] = useState<ShopItem | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClose = () => {
+    navigate('/login');
+  };
 
   useEffect(() => {
     const userId = localStorage.getItem('userId');
-    if (!userId) return;
+    if (!userId) {
+      setIsModalOpen(true);
+      return;
+    }
     (async () => {
       const userResponse = await getUser(userId);
       setShop(userResponse.item.shop?.item ?? null);
@@ -23,6 +34,11 @@ export default function Store() {
         <section className="w-full bg-gray-5 px-12 pt-40 pb-80 md:px-32 md:pt-60 md:pb-120">
           <div className="mx-auto max-w-964"></div>
         </section>
+      )}
+      {isModalOpen && (
+        <Modal onButtonClick={handleClose} onClose={handleClose}>
+          로그인이 필요합니다.
+        </Modal>
       )}
     </>
   );
