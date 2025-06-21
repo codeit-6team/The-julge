45e568ebcb9172bdb44b3c418d1d0923be63ce06
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-21 20:05:58 +0900

    ✨ feat: api 기능 추가

--- src/pages/store/StorePost.tsx
+++ src/pages/store/StorePost.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getShopNotice, type NoticeDetailItem } from '@/api/noticeApi';
 import Footer from '@/components/layout/Footer';
 
 export default function StorePost() {
+  const { shopId, noticeId } = useParams();
+  const [notice, setNotice] = useState<NoticeDetailItem>();
+
+  useEffect(() => {
+    if (!shopId || !noticeId) return;
+
+    (async () => {
+      const { item } = await getShopNotice(shopId, noticeId);
+      setNotice(item);
+    })();
+  }, [shopId, noticeId]);
+
   return (
     <div className="flex min-h-[calc(100vh-108px)] flex-col justify-between bg-gray-5 md:min-h-[calc(100vh_-_70px)]">
       <section className="w-full px-12 py-40 md:px-32 md:py-60"></section>
