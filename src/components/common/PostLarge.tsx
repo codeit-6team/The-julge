11e4bdf0afd47044d6badbca94cbe0c41fdb2245
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-21 18:27:09 +0900

    ✨ feat: noticeDescription 추가

--- src/components/common/PostLarge.tsx
+++ src/components/common/PostLarge.tsx
@@ -24,6 +24,7 @@ export default function PostLarge({ data }: { data: NoticeDetailItem }) {
     workhour,
     startsAt,
     closed,
+    description: noticeDescription,
     shop: {
       item: {
         address1,
@@ -57,66 +58,69 @@ export default function PostLarge({ data }: { data: NoticeDetailItem }) {
   }
 
   return (
-    <div className="flex min-h-356 flex-col items-stretch justify-between rounded-xl border border-gray-20 bg-white p-20 md:p-24 lg:flex-row">
-      <div className="relative h-178 md:h-360 lg:h-auto lg:w-539">
-        <div
-          className="h-full w-full rounded-xl bg-cover bg-center"
-          style={{ backgroundImage: `url(${background})` }}
-        />
-        {status === 'ACTIVE' || (
-          <div className="absolute inset-0 flex items-center justify-center rounded-xl bg-[#000000]/70 text-h3 font-bold text-gray-30 md:text-h1">
-            {overlayText}
-          </div>
-        )}
-      </div>
-      <div className="mt-12 flex flex-col justify-between gap-24 text-black md:mt-16 md:gap-40 lg:w-346 lg:gap-0">
-        <div className="flex flex-col gap-8 text-body2/22 font-regular md:gap-12 md:text-body1/26">
-          <div>
-            <div className="mb-8 leading-17 font-bold text-primary md:leading-20">
-              시급
+    <div className="text-body2/22 font-regular md:text-body1/26">
+      <div className="flex min-h-356 flex-col items-stretch justify-between rounded-xl border border-gray-20 bg-white p-20 md:p-24 lg:flex-row">
+        <div className="relative h-178 md:h-360 lg:h-auto lg:w-539">
+          <div
+            className="h-full w-full rounded-xl bg-cover bg-center"
+            style={{ backgroundImage: `url(${background})` }}
+          />
+          {status === 'ACTIVE' || (
+            <div className="absolute inset-0 flex items-center justify-center rounded-xl bg-[#000000]/70 text-h3 font-bold text-gray-30 md:text-h1">
+              {overlayText}
             </div>
-            <div
-              className="flex items-center gap-4 md:gap-8"
-              title={`${hourlyPay.toLocaleString()}원${isHigherPay ? `: 기존 시급보다 ${percent}%` : ''}`}
-            >
-              <div className="text-h2/29 font-bold md:text-h1/34">
-                {hourlyPay.toLocaleString()}원
+          )}
+        </div>
+        <div className="mt-12 flex flex-col justify-between gap-24 text-black md:mt-16 md:gap-40 lg:w-346 lg:gap-0">
+          <div className="flex flex-col gap-8 md:gap-12">
+            <div>
+              <div className="mb-8 leading-17 font-bold text-primary md:leading-20">
+                시급
               </div>
-              {isHigherPay && status === 'ACTIVE' && (
-                <div
-                  className={`flex h-24 items-center justify-center gap-2 rounded-full px-8 py-4 text-caption/16 text-white md:h-36 md:p-12 md:text-body2 md:font-bold ${badgeBgColor}`}
-                >
-                  <div className="max-w-122 truncate">
-                    기존 시급보다 {percent}%
-                  </div>
-                  <img
-                    src={ic_arrow}
-                    alt="위 화살표"
-                    className="size-16 md:size-20"
-                  />
+              <div
+                className="flex items-center gap-4 md:gap-8"
+                title={`${hourlyPay.toLocaleString()}원${isHigherPay ? `: 기존 시급보다 ${percent}%` : ''}`}
+              >
+                <div className="text-h2/29 font-bold md:text-h1/34">
+                  {hourlyPay.toLocaleString()}원
                 </div>
-              )}
+                {isHigherPay && status === 'ACTIVE' && (
+                  <div
+                    className={`flex h-24 items-center justify-center gap-2 rounded-full px-8 py-4 text-caption/16 text-white md:h-36 md:p-12 md:text-body2 md:font-bold ${badgeBgColor}`}
+                  >
+                    <div className="max-w-122 truncate">
+                      기존 시급보다 {percent}%
+                    </div>
+                    <img
+                      src={ic_arrow}
+                      alt="위 화살표"
+                      className="size-16 md:size-20"
+                    />
+                  </div>
+                )}
+              </div>
             </div>
+            <div className="flex items-center gap-6 text-gray-50">
+              <img
+                src={ic_clock}
+                alt="시계 아이콘"
+                className="size-16 md:size-20"
+              />
+              {dateTime}
+            </div>
+            <div className="flex items-center gap-6 text-gray-50">
+              <img
+                src={ic_location}
+                alt="위치 아이콘"
+                className="size-16 md:size-20"
+              />
+              {address1}
+            </div>
+            {shopDescription}
           </div>
-          <div className="flex items-center gap-6 text-gray-50">
-            <img
-              src={ic_clock}
-              alt="시계 아이콘"
-              className="size-16 md:size-20"
-            />
-            {dateTime}
-          </div>
-          <div className="flex items-center gap-6 text-gray-50">
-            <img
-              src={ic_location}
-              alt="위치 아이콘"
-              className="size-16 md:size-20"
-            />
-            {address1}
-          </div>
-          {shopDescription}
         </div>
       </div>
+      <div>{noticeDescription}</div>
     </div>
   );
 }
