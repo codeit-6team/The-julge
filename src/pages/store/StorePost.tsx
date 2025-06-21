648a7c14c7cec4e0d1a526df99464f63cba53607
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-21 14:02:28 +0900

    ✨ feat: Footer 및 기본 레이아웃 구성

--- src/pages/store/StorePost.tsx
+++ src/pages/store/StorePost.tsx
@@ -1,3 +1,11 @@
+import Footer from '@/components/layout/Footer';
+
 export default function StorePost() {
-  return <div>내 가게 공고 상세 (사장님)</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-108px)] flex-col justify-between bg-gray-5 md:min-h-[calc(100vh_-_70px)]">
+      <section className="w-full px-12 py-40 md:px-32 md:py-60"></section>
+      <section className="w-full flex-1 px-12 pt-40 pb-80 md:px-32 md:py-60"></section>
+      <Footer />
+    </div>
+  );
 }
