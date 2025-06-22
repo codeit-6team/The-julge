526769150acaa0332c07cbcd70ca2cb498fd8869
Moon-ju-young <amymoon07@khu.ac.kr>
2025-06-22 21:39:42 +0900

    🐛 fix: Table 내부 TableButtons props 수정

--- src/components/common/table/Table.tsx
+++ src/components/common/table/Table.tsx
@@ -162,11 +162,7 @@ export default function Table(props: UserProps | NoticeProps) {
                 <td className="sticky right-0 border-b border-l border-gray-20 bg-white px-7 pt-1 md:px-11 @min-[947px]:border-l-transparent">
                   <div className="min-h-44 content-center md:min-h-67">
                     {mode === 'notice' && data[3] === 'pending' ? (
-                      <TableButtons
-                        shopId={props.shopId}
-                        noticeId={props.noticeId}
-                        applicaitonId={data[4] ?? ''}
-                      />
+                      <TableButtons index={index} click={clickButton} />
                     ) : (
                       <TableStatus status={data[3] ?? ''} />
                     )}
